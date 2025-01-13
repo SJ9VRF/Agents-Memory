17d8ca64ea1e0eafcb0e6072dc44d08a0cd7dabf
Arefeh Yavary <yavary.rf@gmail.com>
2025-01-13 08:16:17 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+from agent import Agent
+from state_graph_builder import build_graph
+
+def main():
+    graph = build_graph()
+    agent = Agent(graph)
+    agent.run()
+
+if __name__ == "__main__":
+    main()
 
